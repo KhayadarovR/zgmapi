27a25d66f2e876f0af5760574d495ddc9c7b7ec2
razil <razil_khayka@mail.ru>
2023-04-25 23:25:37 +0300

    Add project files.

---
+++ zgmapi/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using zgmapi.Models;
+
+namespace zgmapi.Controllers
+{
+    [Route("api/[controller]s")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        private static List<User> _users = new List<User>()
+        {
+            new User() { Id = 1, Name = "raz"},
+            new User() { Id = 2, Name = "rus"},
+        };
+
+        // GET users/
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_users);
+        }
+    }
+}
---
+++ zgmapi/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace zgmapi.Models
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+}
